25f937e3b7c54d995ba475eca423e9781c4e9e0d
Jo HyunA <teniuscom@gmail.com>
2024-04-09 19:42:12 +0900

    WorkoutController에 CRUD 연결

--- src/main/java/com/example/workoutlog/controller/WorkoutController.java
+++ src/main/java/com/example/workoutlog/controller/WorkoutController.java
@@ -1,10 +1,14 @@
 package com.example.workoutlog.controller;
 
+import com.example.workoutlog.dto.WorkoutInput;
+import com.example.workoutlog.dto.WorkoutOutput;
 import com.example.workoutlog.service.WorkoutService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,4 +17,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkoutController {
     private final WorkoutService workoutService;
 
+    @PostMapping()
+    public ResponseEntity<WorkoutOutput> create(@RequestBody WorkoutInput workoutInput){
+        if(workoutInput.getExerciseName().isEmpty()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        WorkoutOutput result = workoutService.create(workoutInput);
+
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<WorkoutOutput>> findAll(){
+        return new ResponseEntity<>(workoutService.readAll(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<WorkoutOutput> update(@PathVariable(value="id") Long id, @RequestBody WorkoutInput workoutInput){
+        if(workoutInput.getExerciseName().isEmpty()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        WorkoutOutput result = workoutService.update(id, workoutInput);
+
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable(value = "id") Long id){
+        workoutService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
